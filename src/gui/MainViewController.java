14e8218ceb0f065a561ebcad3aaec11fa65c6e86
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-01-12 20:47:05 -0300

    MainView controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -21,6 +21,19 @@ public class MainViewController implements Initializable {
 			
 	}
 
+	@FXML
+	public void onMenuItemSellerAction () {
+		System.out.println("onMenuItemSellerAction ()");
+	}
 	
+	@FXML
+	public void onMenuItemDepartmentAction () {
+		System.out.println("onMenuItemDepartmentAction ()");
+	}
+	
+	@FXML
+	public void onMenuItemAboutAction () {
+		System.out.println("onMenuItemAboutAction ()");
+	}
 	
 }
