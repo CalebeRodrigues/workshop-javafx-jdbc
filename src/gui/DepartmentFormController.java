2c51804dbf0414e254733263c4658860a163bc70
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-02-02 21:10:20 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -32,7 +35,10 @@ public class DepartmentFormController implements Initializable {
 	private Button btnCancel;
 	
 	private Department entity;
+	
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	public void setDepartment (Department entity) {
 		this.entity = entity;
@@ -40,6 +46,10 @@ public class DepartmentFormController implements Initializable {
 	
 	public void setService(DepartmentService service) {
 		this.service = service;
+	}
+	
+	public void subscripeDataChangeListener (DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 
 	@FXML
@@ -57,6 +67,8 @@ public class DepartmentFormController implements Initializable {
 			
 			service.saveOrUpdate(entity);
 			
+			notifyDataChangeListeners();
+			
 			Utils.currentStage(event).close();
 		}
 		catch (DbException e) {
@@ -65,6 +77,12 @@ public class DepartmentFormController implements Initializable {
 		
 	}
 	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		
